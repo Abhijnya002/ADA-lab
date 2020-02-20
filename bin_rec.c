1a14b0c47bd740da3cec217dbfd2e73bafabb2fa
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2020-02-20 13:03:41 +0530

    Add files via upload

---
+++ bin_rec.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h> 
+int recsearch(int a[],int f, int l, int x) ;
+int recsearch(int a[], int f,int l, int key) 
+{ 
+  	
+	while(f<=l)
+	{
+	int mid=(f+l)/2;
+	if (a[mid]==key) 
+        	return 1; 
+     	if (a[mid] < key) 
+     	return recsearch(a, f,mid-1,key);
+	if (a[mid] > key) 
+     	return recsearch(a, mid+1,l,key); 
+	} 
+} 
+  
+int main() 
+{ 
+    	int n,key,i;
+	printf("enter the number of elements=");
+	scanf("%d",&n);
+	int a[n];
+	printf("enter the elements=\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("enter key=");
+	scanf("%d",&key);
+    	int index = recsearch(a, 0,n-1, key); 
+    	if (index != -1) 
+      	 printf("Element %d is present at index %d", key, index); 
+   	 else
+       	 printf("Element %d is not present", key); 
+    	return 0; 
+} 
+
