49c0e08d18350063c5c7e3dcbeee9247ce78a9d3
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2020-03-04 18:41:48 +0530

    Update linear_rec.c

--- linear_rec.c
+++ linear_rec.c
@@ -1,15 +1,17 @@
-
 #include<stdio.h> 
-int recSearch(int a[], int l, int x) ;
-int recSearch(int a[], int l, int x) 
+
+int recSearch(int arr[], int l, int r, int x) ;
+int recSearch(int arr[], int l, int r, int x) 
 { 
-  
-	if (a[l]!=x) 
-        	return -1; 
-     	if (a[l] == x) 
-        	return l;  
-     	return recSearch(a, l+1,  x); 
+     if (r < l) 
+        return -1; 
+     if (arr[l] == x) 
+        return l; 
+     if (arr[r] == x) 
+        return r; 
+     return recSearch(arr, l+1, r-1, x); 
 } 
+   
   
 int main() 
 { 
@@ -24,11 +26,10 @@ int main()
 	}
 	printf("enter key=");
 	scanf("%d",&key);
-    	int index = recSearch(a, 0, key); 
+    	int index = recSearch(a, 0, n-1,key); 
     	if (index != -1) 
       	 printf("Element %d is present at index %d", key, index); 
    	 else
        	 printf("Element %d is not present", key); 
     	return 0; 
 } 
-
