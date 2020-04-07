22eac436f68ae081b2377c2f62893ce538abda7b
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2020-04-06 17:49:03 -1200

    Update topological.c

--- topological.c
+++ topological.c
@@ -1,43 +1,103 @@
-#include <stdio.h>
- 
-int main(){
-	int i,j,k,n,a[10][10],indeg[10],flag[10],count=0;
- 
-	printf("Enter the no of vertices:\n");
-	scanf("%d",&n);
- 
-	printf("Enter the adjacency matrix:\n");
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++)
-			scanf("%d",&a[i][j]);
-	}
- 
-	for(i=0;i<n;i++){
-        indeg[i]=0;
-        flag[i]=0;
-    }
- 
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            indeg[i]=indeg[i]+a[j][i];
- 
-    printf("\nThe topological order is:");
- 
-    while(count<n){
-        for(k=0;k<n;k++){
-            if((indeg[k]==0) && (flag[k]==0)){
-                printf("%d ",(k+1));
-                flag [k]=1;
-            }
- 
-            for(i=0;i<n;i++){
-                if(a[i][k]==1)
-                    indeg[k]--;
-            }
-        }
- 
-        count++;
-    }
- 
-    return 0;
+#include<stdio.h>
+#define NODE 4
+
+   int stk[100];
+int MAXSIZE = 100; 
+int graph[NODE][NODE] = {
+   {0, 1, 1, 0},
+   {0, 0, 0, 1},
+   {0, 0, 0, 0},
+   {0, 0, 1, 0},
+   
+};
+int top = -1;            
+
+int isempty() {
+
+   if(top == -1)
+      return 1;
+   else
+      return 0;
 }
+   
+int isfull() {
+
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
+}
+
+int peek() {
+   return stk[top];
+}
+
+void pop()
+{
+   int data;
+	
+   if(!isempty()) 
+   {
+      data = stk[top];
+      top = top - 1;   
+      
+   } 
+   else 
+   {
+      printf("Could not retrieve data, Stack is empty.\n");
+   }
+}
+
+void push(int data) {
+
+   if(!isfull()) {
+      top = top + 1;   
+      stk[top] = data;
+   } else {
+      printf("Could not insert data, Stack is full.\n");
+   }
+}
+void topoSort(int u, int visited[],int stk[])
+{
+   visited[u] = 1;               
+
+   for(int v = 0; v<NODE; v++) 
+   {
+      if(graph[u][v])
+      {           
+         if(!visited[v])
+            topoSort(v, visited, stk);
+      }
+   }
+   push(u);     
+
+}
+
+void performTopologicalSort() 
+{
+   int vis[NODE];
+
+   for(int i = 0; i<NODE; i++)
+      vis[i] = 0;     
+
+   for(int i = 0; i<NODE; i++)
+      if(!vis[i])     
+         topoSort(i, vis, stk);
+
+   while(!isempty()) 
+   {
+     printf("%d\t",peek());
+      pop();
+   }
+}
+
+void main() 
+{
+   printf( "Nodes after topological sorted order: ");
+   performTopologicalSort();
+}
+
+
+
+o/p:
+Nodes after topological sorted order: 0 1       3       2    
