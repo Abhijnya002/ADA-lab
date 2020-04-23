5efc48e37ff6f089c90a528f58efee120a454782
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2020-04-23 03:00:07 -1200

    Add files via upload

---
+++ recursive_selection&&insertion.c
@@ -0,0 +1,83 @@
+
+
+#include <stdio.h>
+int minIndex(int a[], int i, int j) 
+{ 
+    if (i == j) 
+        return i; 
+    int k = minIndex(a, i + 1, j); 
+    return (a[i] < a[k])? i : k; 
+} 
+void recurSelectionSort(int a[], int n, int index) 
+{ 
+    
+    if (index == n) 
+       return; 
+    int k = minIndex(a, index, n-1); 
+    if (k != index) 
+    {
+        int temp=a[k];
+        a[k]=a[index];
+        a[index]=temp;
+    }
+    recurSelectionSort(a, n, index + 1); 
+} 
+int main() 
+{ 
+    int arr[] = {13, 21, 5,12, 4, 40}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    recurSelectionSort(arr, n,0); 
+    for (int i = 0; i<n ; i++) 
+        printf("%d\n", arr[i] ); 
+
+    return 0; 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<stdio.h>
+
+void insertionSortRecursive(int arr[], int n) 
+{ 
+    
+    if (n <= 1) 
+        return; 
+    insertionSortRecursive( arr, n-1 ); 
+    int last = arr[n-1]; 
+    int j = n-2; 
+    while (j >= 0 && arr[j] > last) 
+    { 
+        arr[j+1] = arr[j]; 
+        j--; 
+    } 
+    arr[j+1] = last; 
+} 
+void printArray(int arr[], int n) 
+{ 
+    for (int i=0; i < n; i++) 
+        printf("%d\t", arr[i] ); 
+} 
+
+int main() 
+{ 
+    int arr[] = {12, 11, 13, 5, 6}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+  
+    insertionSortRecursive(arr, n); 
+    printArray(arr, n); 
+  
+    return 0; 
+} 
